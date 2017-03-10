651617c804155c2226067507fa0958be59eb61bf
Ahmed Yakout <iyakout@hotmail.com>
2017-03-10 23:04:37 +0200

    added cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/YourProjectName.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/YourProjectName.dir/heap.cpp.o"
+  "YourProjectName.pdb"
+  "YourProjectName"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/YourProjectName.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
